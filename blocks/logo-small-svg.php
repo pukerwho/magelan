0a555da4a724b42b12b8abc50a42b458c736405f
Vladimir Samojlov <v-moi@ya.ru>
2019-04-22 19:34:03 +0300

    edit lang

--- blocks/logo-small-svg.php
+++ blocks/logo-small-svg.php
@@ -17,7 +17,18 @@
                 <g id="Group-6" transform="translate(165.000000, 3.000000)">
                     <g id="logo">
                         <text id="Магелан" font-family="Avenir-Next-Regular" font-size="20" font-weight="normal" fill="#FFFFFF">
-                            <tspan x="63" y="53">МАГЕЛАН</tspan>
+                            
+                                <?php 
+                                $currentlang = get_bloginfo('language');
+                                if($currentlang=="uk"): ?>
+                                    <tspan x="63" y="53">
+                                        МАГЕЛАН
+                                    </tspan>
+                                <?php elseif($currentlang=="en-US"): ?>
+                                    <tspan x="61" y="53">
+                                        MAGELAN
+                                    </tspan>
+                                <?php endif; ?>
                         </text>
                         <path d="M6,48.4637702 C6,48.4637702 26.0028483,36.0725601 21.9394424,0 C21.9394424,0 33.1410375,10.6269954 35.6544844,25.6249057 C36.7729682,32.2980582 35.6544844,44.4348544 25.0435494,48.8933544 C21.2901353,50.465716 12.1411886,52.9222703 6,48.4637702 Z" id="Path" fill="url(#linearGradient-1)"></path>
                         <path d="M38.4821731,46.4965657 C35.7962648,47.6169254 31,49.0428377 31,49.0428377 C31,49.0428377 44.4962721,35.8870989 38.757437,10 C38.757437,10 62.3216945,43.504696 50.8523656,53.6388587 C47.8036095,56.3082005 46.3689007,43.1948996 38.4821731,46.4965657 Z" id="Path" fill="url(#linearGradient-2)"></path>
